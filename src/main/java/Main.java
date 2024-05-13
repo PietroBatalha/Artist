871ed51f70acea5e274c05743099b71595e480bb
PietroBatalha <169068274+PietroBatalha@users.noreply.github.com>
2024-05-13 15:20:06 +0000

    13/05/24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,34 @@
 public class Main {
   public static void main(String[] args) {
    System.out.println("#############"); System.out.println("Playboi Carti");  System.out.println("#############"); 
-    System.out.println(" "); 
-    System.out.println("Álbum:Die Lit");
+    System.out.println(" ");  
+    System.out.println("#############");
+    System.out.println("Álbum:Die Lit"); 
+    System.out.println("#############");
+    System.out.println("");  
+    System.out.println("Die Lit é o segundo álbum do estúdio do rapper Norte-americano Playboi Carti, lançado no dia 11 de maio de 2018. Die lit apresenta uma mistura de punk e trap.. conta com participações de Artistas como como Lil Uzi Vert, Nicki MInaj e Travis Scott. Contém 19 músicas e duração de 57min48s");
+     System.out.println("");
+     System.out.println("################");
+    System.out.println("Músicas do Álbum");
+    System.out.println("#################");
     System.out.println(""); 
+    System.out.println("Long Time - intro (3:31)");
+    System.out.println("R.I.P. (3:12)");
+    System.out.println("Lean 4 Real(feat. Skepta) (2:57)");
+    System.out.println("Old Money (2:15)");
+    System.out.println("Love Hurts(feat. Travis Scott) (3:00)");
+    System.out.println("Shoota(feat. Lil Uzi Vert) (2:33)");
+    System.out.println("Right Now(feat. Pi'erre Bourne) (3:27)");
+    System.out.println("Poke It Out(with Nicki (4:29)");
+    System.out.println("Home (KOD) (2:42)");
+    System.out.println("Fell In Luv (feat. Bryson Tiller (3:26)");
+    
+    
+    
+
+    
+    
+    
   }
   
 }
