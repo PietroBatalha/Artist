11a734e5aea13d79c28c8d19262ff28126aaf61e
PietroBatalha <169068274+PietroBatalha@users.noreply.github.com>
2024-05-13 14:37:35 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+   System.out.println("#############"); System.out.println("Playboi Carti");  System.out.println("#############"); 
+    System.out.println(" "); 
+    System.out.println("Álbum:Die Lit");
+    System.out.println(""); 
+  }
+  
+}
